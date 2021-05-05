db176904068d411b35e0ace0a24663c9956ab4ba
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-04 21:41:54 -0700

    create test for adding keg to the list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+		kegName: 'Guiness',
+		kegBrand: 'stout',
+		kegAlcohol: 7,
+		kegPrice: 5,
+		kegLeftover: 124,
+		id: 1
+	};
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(kegListReducer({}, { type:null })).toEqual({});
+  });
+
+  test('Should add new keg to the mainKegList', () => {
+    const { kegName, kegBrand, kegAlcohol, kegPrice, kegLeftover, id} = kegData;
+    action = {
+      type: c.ADD_KEG,
+      kegName: kegName,
+      kegBrand: kegBrand,
+      kegAlcohol: kegAlcohol,
+      kegPrice: kegPrice,
+      kegLeftover: kegLeftover,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        kegName: kegName,
+        kegBrand: kegBrand,
+        kegAlcohol: kegAlcohol,
+        kegPrice: kegPrice,
+        kegLeftover: kegLeftover,
+        id: id
+      }
+    });
   });
 });
